062f72ba6fb2e64c6168be8ceaf818de8125714b
kswilson2 <kswilson2@ualr.edu>
2025-02-02 01:22:51 +0000

    update

---
+++ 01.05 Previous and Next.py
@@ -0,0 +1,5 @@
+number=int(input("Enter Number: "))
+nextnumber=int(number)+1
+prevnumber=int(number)-1
+print("The next number for the number " + str(number) + " is " + str(nextnumber) + ".")
+print("The previous number for the number " + str(number) + " is " + str(prevnumber) +".")
