35d6bcd412564b4d79bac2d5bbfd2be66501266e
kswilson2 <kswilson2@ualr.edu>
2025-02-02 04:17:01 +0000

    update

---
+++ 01.06 Two Timestamps.py
@@ -0,0 +1,7 @@
+hourone = input("Enter First Timestamp Hours: ")
+minuteone = input("Enter First Timestamp Minutes: ")
+secondone = input("Enter First Timestamp Seconds: ")
+hourtwo = input("Enter Second Timestamp Hours: ")
+minutetwo = input("Enter Second Timestamp Minutes: ")
+secondtwo = input("Enter Second Timestamp Seconds: ")
+
