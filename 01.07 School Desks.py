9d692257ad0dc2eb006cb2a506e7dbae2177fbd0
kswilson2 <kswilson2@ualr.edu>
2025-02-03 16:46:58 +0000

    update

--- 01.07 School Desks.py
+++ 01.07 School Desks.py
@@ -1,1 +1,7 @@
-
+A = int(input("Enter Classroom A: "))
+B = int(input("Enter Classroom B: "))
+C = int(input("Enter Classroom C: "))
+Adesks = A//2 + A%2
+Bdesks = B//2 + B%2
+Cdesks = C//2 + C%2
+print (Adesks + Bdesks + Cdesks)
