3e230f99ca840ebda1f8362ff0aa4fe4c6b4c9fd
kswilson2 <kswilson2@ualr.edu>
2025-02-03 16:24:26 +0000

    update

--- 01.Project.py
+++ 01.Project.py
@@ -1,1 +1,11 @@
 second = int(input("Enter Length of Time in Seconds: "))
+years = second // 31536000
+print(years)
+days = (second // 86400) 
+print(days)
+hours = second // 3600
+print(hours)
+minutes = second // 60
+print(minutes)
+seconds = second
+print(seconds)
---
+++ 03.00.04 Indentation Errors.py
@@ -0,0 +1,7 @@
+x = int(input("Enter a value: "))
+if x > 0:
+    # Need to put four spaces or press <Tab> in the beginning of the next line
+    print(x)  
+else:
+    # Need to put four spaces or press <Tab> in the beginning of the next line
+    print(-x)
