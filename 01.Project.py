d5d12e0e7e1bf9e288a2d6c12aa9ef5fc47d53ef
kswilson2 <kswilson2@ualr.edu>
2025-02-04 19:53:35 +0000

    update

--- 01.Project.py
+++ 01.Project.py
@@ -1,11 +1,15 @@
 second = int(input("Enter Length of Time in Seconds: "))
 years = second // 31536000
-print(years)
-days = (second // 86400) 
-print(days)
-hours = second // 3600
-print(hours)
-minutes = second // 60
-print(minutes)
-seconds = second
-print(seconds)
+print("Years: " + str(years))
+remyrinsec = second % 31536000
+days = remyrinsec // 86400
+print("Days: " + str(days))
+remdayinsec = second % 86400
+hours = remdayinsec // 3600
+print("Hours: " + str(hours))
+remhrinsec = second % 3600
+minutes = remhrinsec // 60
+print("Minutes: " + str(minutes))
+remsec = second % 60
+seconds = remsec
+print("Seconds: " + str(seconds))
