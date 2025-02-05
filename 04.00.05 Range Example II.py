d5a8f0b0d43050609ccc9e7d8d5fe3e7a1600c27
kswilson2 <kswilson2@ualr.edu>
2025-02-05 15:33:01 +0000

    update

--- 04.00.05 Range Example II.py
+++ 04.00.05 Range Example II.py
@@ -1,2 +1,11 @@
 for i in range(5):
     print(i)
+#OR
+maxloop=5
+for counter in range(maxloop):
+    print(counter)
+#OR
+begin=3
+endat=10
+for counter in range(begin,endat):
+    print(counter)
---
+++ 04.00.08 For Loop Negative Step.py
@@ -0,0 +1,3 @@
+for i in range(10, 0, -2):
+    print(i)
+    #starts at 10, adds -2 until it reaches 0
