921dc94a5c5869c0e5278e5834fc776ff1691a1a
kswilson2 <kswilson2@ualr.edu>
2025-02-11 23:58:18 +0000

    update

--- 03.Project.py
+++ 03.Project.py
@@ -1,3 +1,17 @@
 first = float(input("Enter First Number: "))
-operator = input("Enter Operator (+,-.*,/): "))
+operator = input("Enter Operator (+,-,*,/): ")
 second = float(input("Enter Second Number: "))
+if operator == "+":
+    result = first + second
+elif operator == "-":
+    result = first - second
+elif operator == "*":
+    result = first * second
+elif operator == "/":
+    result = first / second
+else:
+    result = None
+if result != None:
+    print(f"{first} {operator} {second} = {result}")
+else: 
+   print("Invalid Operator")
