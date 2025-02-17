d72edaada712237643cf0ad02563030c0bc5521d
kswilson2 <kswilson2@ualr.edu>
2025-02-17 17:33:35 +0000

    update

--- 04.Project.py
+++ 04.Project.py
@@ -1,8 +1,5 @@
 height = int(input("Enter maximum height: "))
-x=0
-for x in range(1, height+1):
-    for i in range(1, x+1):
-        print("*", end="")
-    print("")
-for i in range(height-1, 0, -1):
-    print("*"*i)
+for i in range(1, height+1):
+        print("*"*i)
+for j in range(height-1, 0, -1):
+    print("*"*j)
