1d490a7918aaa2d6a5e65989124ec90c3a5a5a5c
kswilson2 <kswilson2@ualr.edu>
2025-02-24 02:15:28 +0000

    update

---
+++ 05.Projecttrail.py
@@ -0,0 +1,15 @@
+x = int(input("Enter Start of Range:"))
+y = int(input("Enter End of Range: "))
+for i in range(x, y+1):
+    length = 0
+    sum = 0
+    original = i
+    while i > 0:
+        digit = i % 10
+        i //= 10
+        length += 1
+        sum += digit ** length
+        i //= 10
+    if sum == original:
+        print(original)
+    
