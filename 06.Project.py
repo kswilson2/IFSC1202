88c2b3d5dfc56595ba533735d13d68d618aec429
kswilson2 <kswilson2@ualr.edu>
2025-03-05 23:03:23 +0000

    update

--- 06.Project.py
+++ 06.Project.py
@@ -1,2 +1,10 @@
-inputfile = open("06.Project Input File.txt")
+inputfile = open("06.Project Input File.txt", "r")
 outputfile = open("06.Project Output File.txt", "w")
+recordcount = 0
+line = inputfile.readline()
+while line != '':
+    outputfile.write(line)
+    recordcount += 1
+    line = inputfile.readline()
+inputfile.close()
+outputfile.close()
