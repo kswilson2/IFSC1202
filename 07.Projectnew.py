ade871368bc8f1414001892152aa8deac2c4dc5c
kswilson2 <kswilson2@ualr.edu>
2025-03-13 22:03:53 +0000

    update

--- 07.Projectnew.py
+++ 07.Projectnew.py
@@ -1,14 +1,19 @@
 inputfile = open("07.Project Angles Input.txt", "r")
 outputfile = open("07.Project Angles Output.txt", "w")
 outputcount = 0
-read = inputfile.read()
-while read != "":
-    degreeloc = read.find(chr(176))
-    minuteloc = read.find("'")
-    secondloc = read.find('"')
-    degrees = float(read[:degreeloc])
-    minutes = float(read[degreeloc+1:minuteloc])
-    seconds = float(read[minuteloc+1:secondloc])
+line = inputfile.readline()
+while line != "":
+    degreeloc = line.find(chr(176))
+    minuteloc = line.find("'")
+    secondloc = line.find('"')
+    degrees = float(line[:degreeloc])
+    minutes = float(line[degreeloc+1:minuteloc])
+    seconds = float(line[minuteloc+1:secondloc])
     decimaldegrees= degrees + (minutes/60) + (seconds/3600)
+    outputfile.write(str(decimaldegrees))
+    outputcount += 1 
     break
-    print(decimaldegrees) 
+inputfile.close()
+outputfile.close()
+
+    
