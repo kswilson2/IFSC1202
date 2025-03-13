926ff39673e0dba8d1371ae6d544d06685d05d05
kswilson2 <kswilson2@ualr.edu>
2025-03-13 21:49:54 +0000

    update

--- 07.Projectnew.py
+++ 07.Projectnew.py
@@ -1,9 +1,14 @@
-s = str(input("Enter: "))
-degreeloc = s.find(chr(176))
-minuteloc = s.find("'")
-secondloc = s.find('"')
-degrees = float(s[:degreeloc])
-minutes = float(s[degreeloc+1:minuteloc])
-seconds = float(s[minuteloc+1:secondloc])
-decimaldegrees= degrees + (minutes/60) + (seconds/3600)
-print(decimaldegrees)
+inputfile = open("07.Project Angles Input.txt", "r")
+outputfile = open("07.Project Angles Output.txt", "w")
+outputcount = 0
+read = inputfile.read()
+while read != "":
+    degreeloc = read.find(chr(176))
+    minuteloc = read.find("'")
+    secondloc = read.find('"')
+    degrees = float(read[:degreeloc])
+    minutes = float(read[degreeloc+1:minuteloc])
+    seconds = float(read[minuteloc+1:secondloc])
+    decimaldegrees= degrees + (minutes/60) + (seconds/3600)
+    break
+    print(decimaldegrees) 
