191c62644e6e55acf384a4b7169e9fe5e83fcb8e
kswilson2 <kswilson2@ualr.edu>
2025-03-13 23:16:46 +0000

    update

--- 07.Projectnew.py
+++ 07.Projectnew.py
@@ -9,18 +9,17 @@ def ParseDegreeString(ddmmss):
 def DDMMSStoDecimal(degrees, minutes, seconds):
     decimaldegrees= degrees + (minutes/60) + (seconds/3600)
     return decimaldegrees
-
-def file("07.Project Angles Input.txt","07.Project Angles Output.txt"):
-    with open("07.Project Angles Input.txt", "r") as infile:
+inputfile = "07.Project Angles Input.txt"
+outputfile ="07.Project Angles Output.txt"
+def file(inputfile, outputfile):
+    with open(inputfile, "r") as infile:
         lines = infile.readlines()
     outputcount = 0
     for line in lines:
         degrees, minutes, seconds = ParseDegreeString(ddmmss)
         decimaldegrees = DDMMSStoDecimal(degrees, minutes, seconds)
-        outputcount += 1
-    with open("07.Project Angles Output.txt", "w") as outfile:
+        ouputcount.append(decimaldegrees) 
+    with open(outputfile, "w") as outfile:
         for decimaldegrees in outputcount:
-        outputfile.write(f'{decimaldegrees}')
-    return outputcount
-line = inputfile.readline()
-outputcount = 0 
+            outputfile.write(f"{decimaldegrees}")
+    return len(outputcount)
