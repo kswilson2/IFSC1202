48bbaeaf70e738f7b5f609e19848307249a31c69
kswilson2 <kswilson2@ualr.edu>
2025-03-17 15:46:12 +0000

    update

---
+++ 08.02 Odd Elements.py
@@ -0,0 +1,7 @@
+n = input("Enter Values Separated by Spaces:")
+a = []
+s = n.split()
+for x in range(len(s)):
+    a.append(int(s[x]))
+    
+        print(a[x]) 
--- 08.03 Greater Than Previous.py
+++ 08.03 Greater Than Previous.py
@@ -1,4 +1,6 @@
-#split
+n = input("Enter Values Separated by Spaces:")
+a = []
+s = n.split()
 for i in range(1,len(a)):
     if a(i) > a(i-1):
         print a(i)
