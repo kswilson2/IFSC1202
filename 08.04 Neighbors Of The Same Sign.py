936090d8ce1b6d01be64d8de4d182160702d132c
kswilson2 <kswilson2@ualr.edu>
2025-03-17 21:03:13 +0000

    update

--- 08.04 Neighbors Of The Same Sign.py
+++ 08.04 Neighbors Of The Same Sign.py
@@ -1,1 +1,8 @@
-for i in range(1,len(a)-1):
+n = input("Enter Values Separated by Spaces:")
+a = []
+s = n.split()
+for x in range(len(s)):
+    a.append(int(s[x]))
+for i in range(1, len(a)-1):
+    if (a[i] >= 0 and a[i+1] >= 0) or (a[i] < 0 and a[i+1] < 0):
+        print(a[i], a[i+1])   
