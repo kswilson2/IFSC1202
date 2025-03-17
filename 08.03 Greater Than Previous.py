c449e7396e938353c54ad3aa691957e23d44b122
kswilson2 <kswilson2@ualr.edu>
2025-03-17 19:34:14 +0000

    update

--- 08.03 Greater Than Previous.py
+++ 08.03 Greater Than Previous.py
@@ -1,6 +1,6 @@
 n = input("Enter Values Separated by Spaces:")
 a = []
 s = n.split()
-for i in range(1,len(a)):
-    if a(i) > a(i-1):
-        print a(i)
+for i in range(1, len(s)):
+    if s[i] > s[i-1]:
+        print(s[i])
