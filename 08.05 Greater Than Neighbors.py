ba9f98cc8c855f2e63ee72945bada51ee8c2a5ee
kswilson2 <kswilson2@ualr.edu>
2025-03-18 21:57:56 +0000

    update

--- 08.05 Greater Than Neighbors.py
+++ 08.05 Greater Than Neighbors.py
@@ -3,8 +3,9 @@ a = []
 s = n.split()
 for x in range(len(s)):
     a.append(int(s[x]))
+count = 0
 for i in range(1, len(a)-1):
     if (a[i] > a[i-1] and a[i] > a[i+1]):
-        print(len(str(a[i])))
-        #how to print # of occurances 
+        count += 1
+print(count)
  
--- 08.07 Distinct Elements.py
+++ 08.07 Distinct Elements.py
@@ -1,8 +1,11 @@
 n = input("Enter Values Separated by Spaces:")
-s = n.split()
 a = []
+s = n.split()
+for x in range(len(s)):
+    a.append(int(s[x]))
 distinct = 0
-for i in range(len(a)):
+for i in range(len(a)-1):
     if a[i] != a[i+1]:
         distinct += 1
-print(distinct)
+print(f'Number of Distinct Elements: {distinct}')
+#why am I getting 2 instead of 3
