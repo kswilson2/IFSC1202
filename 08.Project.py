f16d6fca94af430dfbe6788d3c9bf7437bce7b14
kswilson2 <kswilson2@ualr.edu>
2025-03-18 22:19:56 +0000

    update

--- 08.Project.py
+++ 08.Project.py
@@ -1,2 +1,12 @@
 #if line contains "liquor," go backwards to find blank and after to find blank
 #line number will be index
+a = []
+inputfile = open("constitution.txt", "r")
+x = inputfile.readline()
+while x != "":
+	a.append(x)
+	x = inputfile.readline()
+inputfile.close()
+# Print the list - get the number of elements using the len() method
+for i in range(len(a)):
+	print(a[i])
