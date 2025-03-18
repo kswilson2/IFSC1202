eb46fe202d459fe42657ced2ee240fcf559f2866
kswilson2 <kswilson2@ualr.edu>
2025-03-18 22:14:23 +0000

    update

--- 08.09 Swap Min and Max.py
+++ 08.09 Swap Min and Max.py
@@ -1,5 +1,24 @@
-#to find min, a[i] < max:
-#find min and max
-#index=i, saves the location 
-#assign minind and maxind
-#swap
+n = input("Enter Values Separated by Spaces:")
+a = []
+s = n.split()
+for x in range(len(s)):
+    a.append(int(s[x]))
+max= a[0] #assume 1st number is max
+maxind = 0
+for i in range(1, len(a)): #find max
+    if a[i] > max: 
+        max = a[i]
+        maxind = i
+print(maxind)
+min= a[0]
+minind = 0
+for i in range(1, len(a)): #find min
+    if a[i] < max: 
+        min = a[i]
+        minind = i
+print(minind)
+temp = a[minind]
+a[minind] = a[maxind]
+a[maxind] = temp
+print(a)
+#formatting trouble
