5de815549ba1cd152152951565e291c7e6234930
kswilson2 <kswilson2@ualr.edu>
2025-04-02 00:50:50 +0000

    update

--- 09.04 Unit Conversion.py
+++ 09.04 Unit Conversion.py
@@ -3,3 +3,17 @@
 #search first column for "to" unit
 #convert to floating point for everything except first row and column
 #similar to project
+FromValue = int(input("Enter From Value: "))
+FromUnit = input("Enter From Unit (mm, cm, m, km, in, yd, mi): ")
+ToUnit = input("Enter To Unit (mm, cm, m, km, in, yd, mi): ")
+a = []
+inputfile = open("09.04 Conversion.txt")
+x = inputfile.readline()
+while x != "":
+	y = x.split(",")
+	a.append(y) 
+	x = inputfile.readline()
+for i in range(len(a)):
+    for j in range(len(a[i])):
+        print(a[i][j], end=' ')
+    print()
