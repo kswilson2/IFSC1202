fded5aacd30fa75c7efd194fb6a78dd8347bacce
kswilson2 <kswilson2@ualr.edu>
2025-04-02 02:24:26 +0000

    update

--- 09.04 Unit Conversion.py
+++ 09.04 Unit Conversion.py
@@ -3,17 +3,34 @@
 #search first column for "to" unit
 #convert to floating point for everything except first row and column
 #similar to project
-FromValue = int(input("Enter From Value: "))
+FromValue = float(input("Enter From Value: "))
 FromUnit = input("Enter From Unit (mm, cm, m, km, in, yd, mi): ")
 ToUnit = input("Enter To Unit (mm, cm, m, km, in, yd, mi): ")
 a = []
 inputfile = open("09.04 Conversion.txt")
 x = inputfile.readline()
 while x != "":
-	y = x.split(",")
+	y = x.split(" ")
+	#for i in range(1, len(y)):
+	#	    y[i] = float(y[i])
 	a.append(y) 
 	x = inputfile.readline()
+
+FromIndex = -1
 for i in range(len(a)):
-    for j in range(len(a[i])):
-        print(a[i][j], end=' ')
-    print()
+	if a[i][0] == FromUnit: 
+		FromIndex = i
+if FromIndex == -1:
+    print("FromUnit is not valid")
+ToIndex = -1
+for i in range(len(a[0])):
+	if a[0][i] == ToUnit:
+		ToIndex = i
+if ToIndex == -1:
+    print("ToUnit is not valid")
+result = round(FromValue * a[FromIndex][ToIndex],7)
+print(result)
+
+	
+		
+    
