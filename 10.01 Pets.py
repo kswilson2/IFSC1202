de586f8a9b3d61f33c2c377a9ac9bd2babe0a43f
kswilson2 <kswilson2@ualr.edu>
2025-04-09 19:29:20 +0000

    update

--- 10.01 Pets.py
+++ 10.01 Pets.py
@@ -1,1 +1,25 @@
 #read, split by comma, just printing values
+
+# Step 1 - Define the class object
+class Pet ():
+	
+# Step 2 - Define the initializer and any default values
+	def __init__(self, name, type, age):
+ 
+# Step 3 - Define the object attributes
+		self.Name = name
+		self.Type = type
+		self.Age = age 
+
+#open input file to read
+inputfile = open("10.01 Pets.txt", "r")
+#create list for objects
+a = []
+line = inputfile.readline()
+while line != "":
+	line = line.strip()
+	a.append(line)
+	line = inputfile.readline()
+
+
+
