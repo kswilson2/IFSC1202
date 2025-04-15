dae41d52f19f62cbb43282bbc3fe468b79d2a898
kswilson2 <kswilson2@ualr.edu>
2025-04-15 22:40:53 +0000

    update

--- 10.Project.py
+++ 10.Project.py
@@ -2,27 +2,25 @@
 
 # Step 1 - Define the class object 
 class Student ():
-	
+
 # Step 2 - Define the initializer and any default values
-	def __init__(self, firstname, lastname, tnumber:
+	def __init__(self, firstname, lastname, tnumber, scores):
  
 # Step 3 - Define the object attributes
-		self.BallType = balltype 
-		self.Diameter = diameter 
-		self.Pressure = pressure 
+		self.FirstName = firstname
+		self.LastName = lastname
+		self.TNumber = tnumber
+		self.Grades = scores
  
 # Step 4 - Define Actions (Methods) associated with the object
-	def Circumference(self):
-		circumference =  pi * self.Diameter
-		return circumference
+	def RunningAverage(self):
+		runningaverage =  
+		return runningaverage
  
 # Step 4 - Here is another action
-	def ChangePressure(self, pressurechange):
-		self.Pressure += pressurechange
-		return self.Pressure
+	def TotalAverage(self):
+
+# Step 4 - Here is another action
+	def LetterGrade(self):
+		
  
-# Step 5 - Create 4 instances of ball
-myball1 = Ball("Basketball", 9.51, 8.0)
-myball2 = Ball("Volleyball", 8.15, 7.5)
-myball3 = Ball("Soccerball", 8.65, 9.0)
-myball4 = Ball() #basketball is assumed using default values
