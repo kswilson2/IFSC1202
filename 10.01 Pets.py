f1523cc4fe3bc66f8c1dc63405a1df0eb8442ec6
kswilson2 <kswilson2@ualr.edu>
2025-04-16 02:52:07 +0000

    update

--- 10.01 Pets.py
+++ 10.01 Pets.py
@@ -12,4 +12,23 @@ class Pet ():
 inputfile = open("10.01 Pets.txt","r")
 #create list for objects
 pets = []
+#read file
+for i in inputfile:
+	line = i.replace("\n","")
+	data = line.split(",")
+#store data
+	temp = Pet() 
+	temp.Name = data[0]
+	temp.Type = data[1]
+	temp.Age = int(data[2])
+	pets.append(temp)
 
+#print header
+print("{:>10} {:>10} {:>10} ".format("Name","Type","Age"))
+
+#print data
+for obj in pets:
+  print("{:>10} {:>10} {:>10} ".format(obj.Name,obj.Type,obj.Age))
+
+#close file
+inputfile.close()
--- 10.02 Retail Item.py
+++ 10.02 Retail Item.py
@@ -1,1 +1,22 @@
-#create method/ action
+class RetailItem ():
+	
+# Step 2 - Define the initializer and any default values
+	def __init__(self, description="", units=0, price=0):
+ 
+# Step 3 - Define the object attributes
+		self.Description = description
+		self.UnitsOnHand = units
+		self.Price = price 
+ 
+# Step 4 - Define Actions (Methods) associated with the object
+	def InventoryValue(self):
+		inventoryvalue =  self.UnitsOnHand * self.Price
+		return inventoryvalue
+ 
+inputfile = open("10.01 Pets.txt","r")
+
+# Step 5 - Create 4 instances of ball
+myball1 = Ball("Basketball", 9.51, 8.0)
+myball2 = Ball("Volleyball", 8.15, 7.5)
+myball3 = Ball("Soccerball", 8.65, 9.0)
+myball4 = Ball() #basketball is assumed using default values
