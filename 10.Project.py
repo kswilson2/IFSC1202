24ca665bf8fc1330eeb5eb5eb63c089ed8f61d0e
kswilson2 <kswilson2@ualr.edu>
2025-04-18 01:47:10 +0000

    update

--- 10.Project.py
+++ 10.Project.py
@@ -1,6 +1,6 @@
 # Step 1 - Define the class object 
 class Student ():
-# Step 2 - Define the initializer and any default values
+# Step 2 - Define the initializer
 	def __init__(self, firstname, lastname, tnumber, scores):
 # Step 3 - Define the object attributes
 		self.FirstName = firstname
@@ -16,7 +16,6 @@ class Student ():
 			else:
 				scores.append(0)
 		return sum(scores)/ len(scores)
-	
 # Step 4 - Here is another action
 	def TotalAverage(self):
 		scores = []
@@ -24,10 +23,9 @@ class Student ():
 			if grade != "":
 				scores.append(int(grade))
 		return sum(scores)/ len(scores)	
-		
 # Step 4 - Here is another action
 	def LetterGrade(self):
-		average = self.RunningAverage
+		average = self.RunningAverage()
 		if average >= 90:
 			return "A"
 		elif average >= 80:
@@ -38,45 +36,21 @@ class Student ():
 			return "D"
 		else:
 			return "F"
-
-inputfile = open("10.Project Student Scores.txt")
-x = inputfile.readline()
-list = []
-while x != "":
-	y = x.split(",")
-	y = x.replace("\n","")
-	student = Student(y[0].strip(), (y[1].strip()), (y[2].strip()), (y[3].strip()))
-	list.append(student)
-	x = inputfile.readline()
-
-#close file
-inputfile.close()
-
-
-#print header
-#print("{:>10} {:>10} {:>10} {:>15} {:>15} {:>10}".format("First","Last","ID","Running","Semester","Letter"))
-#print("{:>10} {:>10} {:>10} {:>15} {:>15} {:>10}".format("Name","Name","Number","Average","Average","Grade"))
-#print("------------ "*6)
-#print data
-
-# for std in list:
-#	print('{:>10}{:>10}{:>14}{:>15.2f}{:>15.2f}{:>10}'.format(std.FirstName, std.LastName, std.TNumber, std.TotalAverage(),std.RunningAverage(),std.LetterGrade()))
-
+#read file and add data
 def main():
-    filename = '10.Project Student Scores.txt' 
-    stud_list = []
-    with open(filename, 'r') as infile:
-        for line in infile:
-            line = line.strip().split(',')
-            s = Student(line[0], line[1], line[2], line[3:])
-            stud_list.append(s)
-
-    print('{:>10}{:>10}{:>14}{:>15}{:>15}{:>10}'.format('First', 'Last', 'ID', 'Running', 'Semester', 'Letter'))
-    print('{:>10}{:>10}{:>14}{:>15}{:>15}{:>10}'.format('Name', 'Name', 'Number', 'Average', 'Average', 'Grade'))
-    print('-' * 75)
-    for std in stud_list:
-        print('{:>10}{:>10}{:>14}{:>15.2f}{:>15.2f}{:>10}'.format(std.FirstName, std.LastName, std.TNumber,
-                                                                  std.TotalAverage(),std.RunningAverage(),
-                                                            std.LetterGrade()))
-main()	
+    inputfile = '10.Project Student Scores.txt' 
+    list = []
+    with open(inputfile, 'r') as file:
+        for x in file:
+            y = x.strip().split(',')
+            s = Student(y[0], y[1], y[2], y[3:])
+            list.append(s)
+#print header
+    print('{:>12}{:>12}{:>12}{:>14}{:>14}{:>12}'.format('First', 'Last', 'ID', 'Running', 'Semester', 'Letter'))
+    print('{:>12}{:>12}{:>12}{:>14}{:>14}{:>12}'.format('Name', 'Name', 'Number', 'Average', 'Average', 'Grade'))
+    print("------------ "*6)
+#print data
+    for std in list:
+        print('{:>12}{:>12}{:>12}{:>14.2f}{:>14.2f}{:>12}'.format(std.FirstName, std.LastName, std.TNumber, std.TotalAverage(),std.RunningAverage(), std.LetterGrade()))
+main()
  
